0c9dc2f461579bac35a90d45844cb4e450ef32c7
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2020-03-18 10:58:49 +0100

    small naming change

--- expense-tracker-rest/models/expense.py
+++ expense-tracker-rest/models/expense.py
@@ -7,7 +7,7 @@ class Expense:
 
     # Returns ALL expenses from a specific user from the database
     def get_user_expenses(self, user_identifier):
-        sql = f"SELECT SUM(ex.amount), u.name AS user_name, c.name AS category_name FROM {self.table_name} AS ex \
+        sql = f"SELECT SUM(ex.amount) as amount, u.name AS user_name, c.name AS category_name FROM {self.table_name} AS ex \
                 INNER JOIN {self.user_table} AS u ON u.id = ex.fk_user \
                 INNER JOIN {self.category_table} AS c ON c.id = ex.fk_category \
                 WHERE u.su = {user_identifier} \
