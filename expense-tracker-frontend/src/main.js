bd26867efc14321baedf84b1688f6b950a8951a9
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2020-03-18 20:41:25 +0100

    install axios; disable CORS policy on backend side;

--- expense-tracker-frontend/src/main.js
+++ expense-tracker-frontend/src/main.js
@@ -3,9 +3,12 @@ import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import GSignInButton from 'vue-google-signin-button'
 import store from './store/store';
+import axios from 'axios';
 
 Vue.use(GSignInButton);
 Vue.use(Vuelidate);
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false;
 
--- expense-tracker-rest/main.py
+++ expense-tracker-rest/main.py
@@ -9,6 +9,15 @@ app = Flask(__name__)
 db = database.Database()
 
 
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+    response.headers['Access-Control-Expose-Headers'] = '*'
+    return response
+
+
 @app.route('/users', methods=['GET'])
 def users():
     def db_query():
